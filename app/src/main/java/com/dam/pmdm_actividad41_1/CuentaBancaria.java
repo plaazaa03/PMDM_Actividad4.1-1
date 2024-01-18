096034b578d5b2749cb1efe32894f2770ca57e5f
Raul Plaza Galvez <raulplaza208@gmail.com>
2024-01-18 12:58:34 +0100

    Comenzado ejercicio 1 de Rafa Moviles

---
+++ app/src/main/java/com/dam/pmdm_actividad41_1/CuentaBancaria.java
@@ -0,0 +1,13 @@
+package com.dam.pmdm_actividad41_1;
+
+import android.content.Context;
+import android.widget.EditText;
+
+// Creacion de la clase Cuenta Bancaria, para luego realizar la herencia a la clase Main.
+public class CuentaBancaria extends androidx.appcompat.widget.AppCompatEditText {
+
+
+    public CuentaBancaria(Context context) {
+        super(context);
+    }
+}
